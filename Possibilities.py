5809c2c91b3562fc14c75d41e498af71ab0af8be
MasterMindNET <romazahrypa@gmail.com>
2018-03-10 18:30:03 +0200

    files write and read & modes

--- Possibilities.py
+++ Possibilities.py
@@ -3,47 +3,37 @@ import sys
 import os
 
 
-def addNumber(fNum, lNum):
-    sumNum = fNum + lNum
-    return sumNum
+'''
+URL(https://stackoverflow.com/questions/16208206/confused-by-python-file-mode-w)
+
+r for reading
+r+ opens for reading and writing (cannot truncate a file)
+w for writing
+w+ for writing and reading (can truncate a file)
+rb for reading a binary file. The file pointer is placed at the beginning of the file.
+rb+ reading or writing a binary file
+wb+ writing a binary file
+a+ opens for appending
+ab+ Opens a file for both appending and reading in binary. The file pointer is at the end of the file if the file exists. The file opens in the append mode.
+x open for exclusive creation, failing if the file already exists (Python 3)
+'''
 
 
-string = addNumber(1, 4)
-print(addNumber(1, 4))
+test_file = open('test.txt', 'ab+')  # wb
+
+print(test_file.mode)
+
+print(test_file.name)
+
+test_file.write(bytes("Write me to the file\n", 'UTF-8'))
+
+test_file.close()
+
+test_file = open('test.txt', 'r+')
+
+text_in_file = test_file.read()
+print(text_in_file)
+
+#os.remove('test.txt')
 
 
-# name = sys.stdin.readline() # user input
-
-#print('Hello', name)
-
-
-long_string = "I'll catch you if you fall - The Floor"
-
-print(long_string[0:4])
-
-print(long_string[-5:])
-
-print(long_string[:-5]) # I'll catch you if you fall - The
-
-print(long_string[:4] + ' be there')
-
-print("%c is my %s letter and my number %d is %.5f" %
-      ('X', 'favorite' , 1, .14))
-
-print(long_string.capitalize())
-
-print(long_string.find("Floor")) # case sensitive
-
-print(long_string.isalpha())
-
-print(long_string.isalnum())
-
-print(len(long_string))
-
-print(long_string.replace("Floor", "Ground"))
-
-print(long_string.strip())
-
-quote_list = long_string.split(" ")
-
-print(quote_list)
