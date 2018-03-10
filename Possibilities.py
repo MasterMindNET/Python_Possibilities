db2395b9627ba6ce92d3baf8426ea462bb62474c
MasterMindNET <romazahrypa@gmail.com>
2018-03-10 16:34:42 +0200

    nums, strings, print()

--- Possibilities.py
+++ Possibilities.py
@@ -3,19 +3,32 @@ import sys
 import os
 
 print("Hello World")
-ammed commit
 # comment
+'''
+multiline comment
+'''
+name = 'Derek'
+name2 = "Derek2"
+print(name)
+number = 15
+# Numbers Strings Lists Tuples Dictionaries
 
+'''
+operations: + - * / % ** //
+** - exponentiation
+// - round off to floor 5/2 = 2, but not 2.5
+'''
+print('5 ** 2 =', 5 + 2)
+print('5 // 2 =', 5 // 2)
 
+quote = "\"Always remember you are unique"
 
+multi_line_quote = '''just
+like else else'''
 
+print("%s %s %s" % ('I like the quote', quote, multi_line_quote))
 
+print('\n' * 5)
 
-
-
-
-
-
-
-
-
+print("i don't like ", end="") # remove new line
+print("newlines")
