3a478f2c04850c02440207ad7748821263eb4117
MasterMindNET <romazahrypa@gmail.com>
2018-03-10 18:01:18 +0200

    while loop

--- Possibilities.py
+++ Possibilities.py
@@ -2,20 +2,23 @@ import random
 import sys
 import os
 
-for x in range(0, 10):
-    print(x, ' ', end="")  # output x indet with space and no-wrap
+random_num = random.randrange(0, 100)
 
-print('\n')
+while(random_num != 15):
+    print(random_num)
+    random_num = random.randrange(0, 100)
 
-grocery_list = ['Juice', 'Tomatoes', 'Potatoes', 'Bananas']
+i = 0
 
-for y in grocery_list:
-    print(y)
+while(i <= 20):
+    if(i % 2 == 0):
+        print(i)
+    elif (i == 9):
+        break
+    else:
+        i+=1
+        continue
 
-for x in [2, 4, 6, 8, 10]:
-    print(x)
+    i+=1
 
-num_list = [[1, 2, 3], [10, 20, 30], [100, 200, 330]]
-for x in range(0, len(num_list)): # range(0, 3)
-    for y in range(0, len(num_list)):
-        print(num_list[x][y])
+print(i)
