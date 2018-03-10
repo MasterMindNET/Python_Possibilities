453ac2dbe8e61c29c474532a4bbb2dd36af7eaa5
MasterMindNET <romazahrypa@gmail.com>
2018-03-10 18:17:02 +0200

    defining function and working with string`s functions

--- Possibilities.py
+++ Possibilities.py
@@ -2,23 +2,48 @@ import random
 import sys
 import os
 
-random_num = random.randrange(0, 100)
 
-while(random_num != 15):
-    print(random_num)
-    random_num = random.randrange(0, 100)
+def addNumber(fNum, lNum):
+    sumNum = fNum + lNum
+    return sumNum
 
-i = 0
 
-while(i <= 20):
-    if(i % 2 == 0):
-        print(i)
-    elif (i == 9):
-        break
-    else:
-        i+=1
-        continue
+string = addNumber(1, 4)
+print(addNumber(1, 4))
 
-    i+=1
 
-print(i)
+# name = sys.stdin.readline() # user input
+
+#print('Hello', name)
+
+
+long_string = "I'll catch you if you fall - The Floor"
+
+print(long_string[0:4])
+
+print(long_string[-5:])
+
+print(long_string[:-5]) # I'll catch you if you fall - The
+
+print(long_string[:4] + ' be there')
+
+print("%c is my %s letter and my number %d is %.5f" %
+      ('X', 'favorite' , 1, .14))
+
+print(long_string.capitalize())
+
+print(long_string.find("Floor")) # case sensitive
+
+print(long_string.isalpha())
+
+print(long_string.isalnum())
+
+print(len(long_string))
+
+print(long_string.replace("Floor", "Ground"))
+
+print(long_string.strip())
+
+quote_list = long_string.split(" ")
+
+print(quote_list)
